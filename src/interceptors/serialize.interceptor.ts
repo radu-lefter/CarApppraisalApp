62157e55eec9b2a899bc54b4bbf414b2776f27e0
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-05 19:23:04 +0000

    completed implementing user interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,6 +7,14 @@ import {
   import { Observable } from 'rxjs';
   import { map } from 'rxjs/operators';
   import { plainToInstance } from 'class-transformer';
+
+  interface ClassConstructor {
+    new (...args: any[]): {};
+  }
+
+  export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+  }
   
   export class SerializeInterceptor implements NestInterceptor {
     constructor(private dto: any) {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,16 +8,15 @@ import {
   Param,
   Query,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -26,10 +25,9 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
