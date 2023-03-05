e51be2afbc5cb1d624bbed83d3625111a1b31c48
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-05 19:10:52 +0000

    updated interceptor to receive any type of dto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,18 +6,17 @@ import {
   } from '@nestjs/common';
   import { Observable } from 'rxjs';
   import { map } from 'rxjs/operators';
-  import { plainToClass } from 'class-transformer';
+  import { plainToInstance } from 'class-transformer';
   
   export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-      // Run something before a request is handled
-      // by the request handler
-      console.log('Im running before the handler', context);
-  
       return handler.handle().pipe(
         map((data: any) => {
-          // Run something before the response is sent out
-          console.log('Im running before response is sent out', data);
+          return plainToInstance(this.dto, data, {
+            excludeExtraneousValues: true,
+          });
         }),
       );
     }
