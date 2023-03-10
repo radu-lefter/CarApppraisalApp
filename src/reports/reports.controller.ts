80652c2f29be090525ed97d76114ebc622690ef2
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2023-03-10 13:44:41 +0000

    implemented controller and service to set reports as approved or not

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
@@ -6,15 +13,21 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
-    constructor(private reportsService: ReportsService) {}
+  constructor(private reportsService: ReportsService) {}
 
-    @Post()
-    @UseGuards(AuthGuard)
-    @Serialize(ReportDto)
-    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
-      return this.reportsService.create(body, user);
-    }
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
 }
